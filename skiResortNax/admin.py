a7afad48368ba7e642cd84fbc6cce600e71ae668
Tink711 <37997290+Tink711@users.noreply.github.com>
2019-05-17 09:45:48 +0200

    add models in admin.py

--- skiResortNax/admin.py
+++ skiResortNax/admin.py
@@ -1,3 +1,11 @@
-from django.contrib import admin
-
+from django.contrib.gis import admin
+from .models import Restaurant, Grenouillere, Slope, Lift, Parking
 # Register your models here.
+
+admin.site.register(Lift, admin.OSMGeoAdmin)
+admin.site.register(Restaurant,  admin.OSMGeoAdmin)
+admin.site.register(Slope,  admin.OSMGeoAdmin)
+admin.site.register(Grenouillere,  admin.OSMGeoAdmin)
+admin.site.register(Parking,  admin.OSMGeoAdmin)
+
+
